45a017fd9e9cd1603e027b3f4a46ee16eca8dc67
Даниил Шило <tokiory.personal@gmail.com>
2023-11-13 15:33:56 +0300

    feat(browser): add search

--- src/components/BrowserFilter/BrowserFilter.tsx
+++ src/components/BrowserFilter/BrowserFilter.tsx
@@ -1,10 +1,18 @@
 import {QuestionTag} from "@/components/QuestionTag";
+import {Input, Text} from "@/components/ui";
 import filterStore from "@/stores/filterStore.ts";
 import {useStore} from "@nanostores/react";
+import type {FormEventHandler} from "react";
 import styles from './browserfilter.module.scss';
+import * as internalTags from '@/data/tags';
 
 export const BrowserFilter = () => {
   const filters = useStore(filterStore.state);
+
+  const onSearchInput: FormEventHandler<HTMLInputElement> = (event) => {
+    const input = event.target as HTMLInputElement;
+    filterStore.search.update(input.value);
+  }
 
   const tags = <div className={styles.tags}>
     {filters.tags.map(tag =>
@@ -16,9 +24,23 @@ export const BrowserFilter = () => {
       />)}
   </div>;
 
+  const suggestion = <div className={styles.suggestions}>
+    <Text size="sm" className={styles.suggestions__text}>
+      Попробуйте следующие теги:
+    </Text>
+    <ul className={styles.suggestions__list}>
+      {
+        Object.values(internalTags).map(tags => {
+          return Object.values(tags).map(tag => <li><QuestionTag onClick={tag => filterStore.tags.toggle(tag)} key={tag.slug} {...tag} /></li>)
+        })
+      }
+    </ul>
+  </div>
+
   return (
     <div className={styles.browserFilter}>
-      {tags}
+      <Input placeholder={'Поиск'} value={filters.search} onInput={onSearchInput}  />
+      {filters.tags.length > 0 ? tags : suggestion}
     </div>
   );
 };
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,3 +2,4 @@ export * from './Button';
 export * from './Text';
 export * from './Logo';
 export * from './Title';
+export * from './Input';
